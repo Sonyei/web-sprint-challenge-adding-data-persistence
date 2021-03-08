f9c40b744e6c1f4e89441249d51c47a7ba248b70
Angelika Blackburn <esonyei@gmail.com>
2021-03-07 22:20:29 -0500

    case when requries a whole new dependency

--- data/migrations/20210307235517_initial.js
+++ data/migrations/20210307235517_initial.js
@@ -3,7 +3,7 @@ exports.up = async function (knex) {
 		table.increments("project_id");
 		table.text("project_name").notNull();
 		table.text("project_description");
-		table.boolean("project_completed").notNull().defaultTo(knex.raw("false"));
+		table.boolean("project_completed").notNull().defaultTo(0);
 	});
 
 	await knex.schema.createTable("resources", (table) => {
@@ -16,7 +16,7 @@ exports.up = async function (knex) {
 		table.increments("task_id");
 		table.text("task_description").notNull();
 		table.text("task_notes");
-		table.boolean("task_completed").notNull().defaultTo(knex.raw("false"));
+		table.boolean("task_completed").notNull().defaultTo(0);
 		table
 			.integer("project_id")
 			.notNull()
